a8da93fe01bf37e237be07078c556419a5e27442
Bob van der Linden <bobvanderlinden@gmail.com>
2021-07-28 09:46:02 +0200

    wip

--- home/default.nix
+++ home/default.nix
@@ -200,7 +200,40 @@ in {
     };
 
     # Desktop notifications
-    services.dunst.enable = true;
+    services.dunst = {
+      enable = true;
+      settings = rec {
+        global = {
+          markup = "none";
+          format = ''
+            <big><b>%s</b></big>
+            %b
+          '';
+          sort = false;
+          alignment = "left";
+          bounce_freq = 0;
+          word_wrap = true;
+          ignore_newline = false;
+          geometry = "450x100-15+49";
+          transparency = 10;
+          separator_height = 2;
+          padding = 12;
+          horizontal_padding = 20;
+          line_height = 3;
+          separator_color = "frame";
+          frame_width = 2;
+          frame_color = "#EC5F67";
+        };
+
+        urgency_normal = {
+          foreground = "#CDD3DE";
+          background = "#101010";
+          timeout = 6;
+        };
+        urgency_low = urgency_normal;
+        urgency_critical = urgency_normal;
+      };
+    };
 
     programs.kitty = {
       enable = true;
