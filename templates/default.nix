767eaf63b5b3908512f660070246cf9914c4b88a
Bob van der Linden <bobvanderlinden@gmail.com>
2022-09-20 12:00:33 +0200

    add dev-shell template

--- templates/default.nix
+++ templates/default.nix
@@ -1,4 +1,9 @@
 {
+  dev-shell = {
+    path = ./dev-shell;
+    description = "A minimal dev-shell flake";
+  };
+
   package = {
     path = ./package;
     description = "A minimal package";
---
+++ templates/dev-shell/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+
+          ];
+        };
+      }
+    );
+}
