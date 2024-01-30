296a41cb7a9bbda6fc1afa0accf04b52d252870f
Bob van der Linden <bobvanderlinden@gmail.com>
2024-01-30 19:49:22 +0100

    remove gnome-dbus-emulation-wlr

--- packages/default.nix
+++ packages/default.nix
@@ -5,7 +5,6 @@ in
 rec {
   coin = callPackage ./coin { };
   git-worktree-shell = callPackage ./git-worktree-shell { };
-  gnome-dbus-emulation-wlr = callPackage ./gnome-dbus-emulation-wlr { };
   immersed = callPackage ./immersed { };
   disable-firewall = callPackage ./disable-firewall { };
   xrdesktop = callPackage ./xrdesktop { };
