66675ea9b9f613fa9b7e9600ecd748166e44d594
Bob van der Linden <bobvanderlinden@gmail.com>
2024-10-19 14:12:17 +0200

    remove gnome-shell-xrdesktop (failing build)

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,7 @@
 let
   inherit (pkgs) callPackage;
 in
-rec {
+{
   coin = callPackage ./coin { };
   git-worktree-shell = callPackage ./git-worktree-shell { };
   wl-screenrecord = callPackage ./wl-screenrecord { };
@@ -24,31 +24,5 @@ rec {
   sk_gpu = callPackage ./sk_gpu { };
   reactphysics3d = callPackage ./reactphysics3d { };
   libinputsynth = callPackage ./libinputsynth { mutter = pkgs.mutter338; };
-  gnome-shell-xrdesktop = pkgs.gnome-shell.overrideAttrs (oldAttrs: {
-    src = pkgs.fetchFromGitLab {
-      domain = "gitlab.freedesktop.org";
-      owner = "xrdesktop";
-      repo = "gnome-shell";
-      rev = "d3594f643407c6fd1e8f4b50112833abf60433a1";
-      hash = "sha256-IzmvqKrbeS2O/d2GlFCYXZOaUGj/5oYRYfhQyX63xxE=";
-    };
-
-    postPatch = ''
-      patchShebangs src/data-to-c.pl
-
-      # We can generate it ourselves.
-      rm -f man/gnome-shell.1
-    '';
-
-    buildInputs =
-      oldAttrs.buildInputs
-      ++ (with pkgs; [ vulkan-loader ])
-      ++ [
-        xrdesktop
-        gxr
-        gulkan
-        libinputsynth
-      ];
-  });
   sejda = callPackage ./sejda { };
 }
