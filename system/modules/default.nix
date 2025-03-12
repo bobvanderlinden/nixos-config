74541dbbafe3677e3206dd6d57ae3ccccf9ef277
Bob van der Linden <bobvanderlinden@gmail.com>
2025-03-12 15:35:52 +0100

    remove i3 suite

--- system/modules/default.nix
+++ system/modules/default.nix
@@ -1,6 +1,5 @@
 {
   suite-single-user = import ./suites/single-user.nix;
-  suite-i3 = import ./suites/i3.nix;
   suite-sway = import ./suites/sway.nix;
   suite-wayland = import ./suites/wayland.nix;
   suite-nix-channels = import ./suites/nix-channels.nix;
