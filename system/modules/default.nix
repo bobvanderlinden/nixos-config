3a20009b9085978a3619e9fdbf77a8e977b8fa50
Bob van der Linden <bobvanderlinden@gmail.com>
2026-09-09 11:01:28 +0200

    fixup! nix: collect modules automatically

--- system/modules/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  "greetd-autologin-keyring" = import ./greetd-autologin-keyring.nix;
-  suite-single-user = import ./suite-single-user;
-  suite-nix-channels = import ./suite-nix-channels;
-  home-manager = import ./home-manager.nix;
-  wireguard = import ./wireguard.nix;
-}
